c07db254dcfbae0808416b520d2b652cb89359fb
Neil Huyton <neil.huyton@gmail.com>
2018-12-11 18:25:49 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+// first we will make a new context
+const MyContext = React.createContext();
+
+// Then create a provider Component
+class MyProvider extends Component {
+  state = {
+    name: "Wes",
+    age: 100,
+    cool: true
+  };
+  render() {
+    return (
+      <MyContext.Provider
+        value={{
+          state: this.state,
+          growAYearOlder: () =>
+            this.setState({
+              age: this.state.age + 1
+            })
+        }}
+      >
+        {this.props.children}
+      </MyContext.Provider>
+    );
+  }
+}
+
+const Family = props => (
+  <div className="family">
+    <Person />
+  </div>
+);
+
+class Person extends Component {
+  render() {
+    return (
+      <div className="person">
+        <MyContext.Consumer>
+          {context => (
+            <React.Fragment>
+              <p>Age: {context.state.age}</p>
+              <p>Name: {context.state.name}</p>
+              <button onClick={context.growAYearOlder}>🍰🍥🎂</button>
+            </React.Fragment>
+          )}
+        </MyContext.Consumer>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MyProvider>
+        <div>
+          <p>I am the app</p>
+          <Family />
+        </div>
+      </MyProvider>
     );
   }
 }
